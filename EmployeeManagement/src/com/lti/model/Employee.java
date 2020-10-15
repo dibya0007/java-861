75d0bc9df20bd1af296aed1fc7f270c30ed5d25e
DIBYASREE <DIBYASREE@LAPTOP-QEH9UTLD>
2020-10-15 12:41:32 +0530

    added employee class

---
+++ EmployeeManagement/src/com/lti/model/Employee.java
@@ -0,0 +1,24 @@
+package com.lti.model;
+
+public class Employee {
+	private int id;
+	private String Name;
+	
+	public Employee() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+
+}
