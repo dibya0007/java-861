97c01aae894a92c40ebf97716a2c631654a0e972
DIBYASREE <DIBYASREE@LAPTOP-QEH9UTLD>
2020-10-15 13:12:41 +0530

    Added project class

---
+++ EmployeeManagement/src/com/lti/model/project.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class project {
+	int projectcode;
+
+	public project(int projectcode) {
+		super();
+		this.projectcode = projectcode;
+	}
+
+	public int getProjectcode() {
+		return projectcode;
+	}
+
+	public void setProjectcode(int projectcode) {
+		this.projectcode = projectcode;
+	}
+	
+}
